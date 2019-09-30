50683b07817bf389cbfb35ed3db377b1ffb357b2
Marc Gallagher <marc@ammoseek.com>
2019-09-29 21:07:23 -0400

    added sitemap and pwa manifest plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,11 @@ module.exports = {
       title: `AmmoSeek Blog`,
       description: `Welcome to the new AmmoSeek Blog. Here is where we'll keep you up to date with all the news, tips, tricks, features, offers, and more!`,
       author: `@gatsbyjs`,
+      siteUrl: 'http://blog.ammoseek.com/'
    },
    plugins: [
       `gatsby-plugin-react-helmet`,
+      'gatsby-plugin-sitemap',
       {
          resolve: `gatsby-source-filesystem`,
          options: {
@@ -23,15 +25,14 @@ module.exports = {
       'gatsby-transformer-remark',
       `gatsby-transformer-sharp`,
       `gatsby-plugin-sharp`,
-
       {
          resolve: `gatsby-plugin-manifest`,
          options: {
-            name: `gatsby-starter-default`,
-            short_name: `starter`,
+            name: `AmmoSeek Blog`,
+            short_name: `AmmoSeek`,
             start_url: `/`,
-            background_color: `#663399`,
-            theme_color: `#663399`,
+            background_color: `#D31918`,
+            theme_color: `#D31918`,
             display: `minimal-ui`,
             icon: `src/images/AS-Circle-Logo-For-LightBackgrounds-128x128.png`, // This path is relative to the root of the site.
          },
@@ -39,7 +40,7 @@ module.exports = {
       'gatsby-plugin-styled-components',
       // this (optional) plugin enables Progressive Web App + Offline functionality
       // To learn more, visit: https://gatsby.dev/offline
-      // `gatsby-plugin-offline`,
+      `gatsby-plugin-offline`,
       'gatsby-plugin-netlify',
    ],
 }
