ded38da80e45827378a5b4375b27fd8b0139db62
Marc Gallagher <marc@ammoseek.com>
2019-09-30 15:52:57 -0400

    add offset title style with ammoseek red color bg

--- src/components/listing.js
+++ src/components/listing.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
-import Styled from "styled-components"
+import Styled from 'styled-components'
 
 const PostCSS = Styled.article`
    box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.2);
    background-color: lightgray;
    padding: 1rem;
+   padding-top: 0;
    border-radius: 5px;
    margin-bottom: 1rem;
+   h2 {
+      background-color: #D31918;
+      overflow: visible;
+      margin-left: -1.5rem;
+      border-top-left-radius: 5px;
+      border-top-right-radius: 5px;
+      border-bottom-right-radius: 5px;
+      border-bottom-left-radius: 5px;
+      padding: 0.5rem;
+      padding-left: 1rem;
+      width: calc(100% + 3rem);
+      box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.5);
+   }
    a {
       color: #000;
       text-decoration: none;
@@ -49,16 +63,21 @@ const Listing = () => {
 
    return (
       <>
-      {data.allMarkdownRemark.edges.map( ({node}) => (
-         <PostCSS key={node.frontmatter.slug}>
-            <Link to={`/posts${node.frontmatter.slug}`}>
-               <h2>{node.frontmatter.title}</h2>
-            </Link>
-            <p>{node.frontmatter.date}</p>
-            <p>{node.excerpt}</p>
-            <Link className="read-more" to={`/posts${node.frontmatter.slug}`}>Read More...</Link>
-         </PostCSS>
-      ))}
+         {data.allMarkdownRemark.edges.map(({ node }) => (
+            <PostCSS key={node.frontmatter.slug}>
+               <Link to={`/posts${node.frontmatter.slug}`}>
+                  <h2>{node.frontmatter.title}</h2>
+               </Link>
+               <p>{node.frontmatter.date}</p>
+               <p>{node.excerpt}</p>
+               <Link
+                  className="read-more"
+                  to={`/posts${node.frontmatter.slug}`}
+               >
+                  Read More...
+               </Link>
+            </PostCSS>
+         ))}
       </>
    )
 }
